38cd7ebae08f7d9a0d3e313d4b568fcb2005bb2b
Ghali Dhobb <ghalidhobb@gmail.com>
2022-06-23 20:50:33 +0200

    banner mobile et footer mobile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'contact', to: 'pages#contact'
   get 'menu', to: 'pages#menu'
-  get 'gallerie', to: 'pages#gallerie'
+  get 'galerie', to: 'pages#galerie'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

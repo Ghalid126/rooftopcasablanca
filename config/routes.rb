927498f18c9f9cf2917023371134f78a23e8dc6f
Ghali Dhobb <ghalidhobb@gmail.com>
2022-06-23 17:33:17 +0200

    navbar logo horizontal, navbar Réservations, navbar & page gallerie, logo middle banner

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'contact', to: 'pages#contact'
   get 'menu', to: 'pages#menu'
+  get 'gallerie', to: 'pages#gallerie'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
